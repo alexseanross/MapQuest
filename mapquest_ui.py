aa0b0ae5df56b751b59df44c26929cc61624cf0a
CEMBacho <cristianedward.bacho.cics@ust.edu.ph>
2022-10-15 08:31:37 +0800

    MapQuest_UI_V3
    Directions showing in popup, FInal touches

--- mapquest_ui.py
+++ mapquest_ui.py
@@ -5,11 +5,10 @@ import datetime
 
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "rT6wCu7ekTjG9wRb42Cg4NBJ1SkuF8hs"
-
 sg.theme("DarkAmber")
 
 x = datetime.datetime.now()
-#THIS IS A COMMENT
+
 layout = [
 
     [sg.Text(x.strftime("%X"))],
@@ -22,55 +21,73 @@ layout = [
 ]
 
 window = sg.Window('Get Directions', layout)
-event, values = window.read()
-url = main_api + urllib.parse.urlencode({"key":key, "from":values[0], "to":values[1]})
-window.close()
 
-json_data = requests.get(url).json()
-json_status = json_data["info"]["statuscode"]
+while True: 
+    event, values = window.read()
+    url = main_api + urllib.parse.urlencode({"key":key, "from":values[0], "to":values[1]})
+    window.close()
 
-if json_status == 0:
-     print("API Status: " + str(json_status) + " = A successful route call.\n")
-     
-     sg.popup( 
+    json_data = requests.get(url).json()
 
+    json_status = json_data["info"]["statuscode"]
+
+
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
         "URL: " + (url),
         "Directions from: " + values[0] + " to " + values[1],
         "Trip Duration: " + (json_data["route"]["formattedTime"]),
         "Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)),
         "Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)),
+        directions = " "
+        for each in json_data["route"]["legs"][0]["maneuvers"]:
+            print ((each["narrative"]) + " (" + str ("{:.2f}".format ((each["distance"])*1.61) + " km) "))
+            route = (each["narrative"]) + " (" + str ("{:.2f}".format ((each["distance"])*1.61) + " km) ")
+            directions = (directions + "\n" + route)
+
+        sg.popup_scrolled (
+        
+        
+        'Directions from ' + values[0] + ' to ' + values[1],
+        "Trip Duration: " + (json_data["route"]["formattedTime"]),
+        "Kilometer: " + str ("{:.2f}".format((json_data["route"]["distance"])*1.61)),
+        "Fuel Used (Ltr): " + str ("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)),
+        directions,
+        size = (70,15),
         title = "Travel Information"
-
-        )  
-
-elif json_status == 402:
-        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
+        )
         
-        sg.popup(
-            
-            "Invalid user inputs for one or both locations.",
-            title = "Something went wrong"
-            
-            )
 
-elif json_status == 611:
-        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
-        
-        sg.popup(
             
-            "Missing an entry for one or both locations.",
-            title = "Something went wrong"
+
+    elif json_status == 402:
+            print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
             
-            )
+            sg.popup(
+                
+                "Invalid user inputs for one or both locations.",
+                title = "Something went wrong"
+                
+                )
 
-else:
-        print("For Staus Code: " + str(json_status) + "; Refer to:")
-        print("https://developer.mapquest.com/documentation/directions-api/status-codes")
-        
-        sg.popup(
+    elif json_status == 611:
+            print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
+            
+            sg.popup(
+                
+                "Missing an entry for one or both locations.",
+                title = "Something went wrong"
+                
+                )
 
-            "Staus Code: " + str(json_status),
-            "Refer to: https://developer.mapquest.com/documentation/directions-api/status-codes",
-            title = "Something went wrong"
+    else:
+            print("For Staus Code: " + str(json_status) + "; Refer to:")
+            print("https://developer.mapquest.com/documentation/directions-api/status-codes")
+            
+            sg.popup(
 
-            )
+                "Staus Code: " + str(json_status),
+                "Refer to: https://developer.mapquest.com/documentation/directions-api/status-codes",
+                title = "Something went wrong"
+
+                )
